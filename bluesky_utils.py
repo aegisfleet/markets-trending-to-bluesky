d50d2051c3aa94e4334fd35c0b24e6db4a69e4a3
aegisfleet <aegisfleet@gmail.com>
2024-04-21 23:25:18 +0900

    画像サイズを小さくして投稿するよう修正

--- bluesky_utils.py
+++ bluesky_utils.py
@@ -1,7 +1,9 @@
-import httpx
+import io
 import requests
-from bs4 import BeautifulSoup
+import time
 from atproto import models, client_utils
+from bs4 import BeautifulSoup
+from PIL import Image
 
 def fetch_webpage_metadata(url):
     try:
@@ -32,10 +34,37 @@ def format_message_with_link(title, url, introduction, content):
     formatted_content = content.replace("\n", "").replace("。", "。\n")
     return client_utils.TextBuilder().text(f"{introduction}\n\n").link(title, url).text(f"\n{formatted_content}")
 
+def compress_image(image_bytes, max_size_kb=500, quality=85):
+    img = Image.open(io.BytesIO(image_bytes))
+
+    if img.mode == 'RGBA' or img.mode == 'P':
+        img = img.convert('RGB')
+
+    while True:
+        with io.BytesIO() as output:
+            img.save(output, format="JPEG", quality=quality)
+            compressed_bytes = output.getvalue()
+            size_kb = len(compressed_bytes) / 1024
+            if size_kb <= max_size_kb:
+                return compressed_bytes
+
+        if quality > 20:
+            quality -= 5
+        else:
+            img = img.resize((img.width * 9 // 10, img.height * 9 // 10), resample=Image.Resampling.LANCZOS)
+
 def create_external_embed(bs_client, title, description, url, img_url):
     trimmed_desc = description.replace("\n", "")[:200]
-    img_data = httpx.get(img_url).content
-    thumb_blob = bs_client.upload_blob(img_data).blob
+    try:
+        img_response = requests.get(img_url)
+        img_response.raise_for_status()
+        img_data = img_response.content
+    except requests.RequestException as e:
+        print(f"画像データの取得に失敗しました: {e}")
+        return None
+
+    compressed_img_data = compress_image(img_data)
+    thumb_blob = bs_client.upload_blob(compressed_img_data).blob
     return models.AppBskyEmbedExternal.Main(
         external=models.AppBskyEmbedExternal.External(
             title=title,
@@ -46,4 +75,13 @@ def create_external_embed(bs_client, title, description, url, img_url):
     )
 
 def post(bs_client, text, embed):
-    bs_client.send_post(text, embed=embed)
+    retries = 3
+    for attempt in range(1, retries + 1):
+        try:
+            bs_client.send_post(text, embed=embed)
+            break
+        except Exception as e:
+            print(f"送信に失敗しました。リトライします... リトライ回数: {attempt}, エラー: {e}")
+            time.sleep(3)
+    else:
+        print("リトライ上限に達しました。送信に失敗しました。")
